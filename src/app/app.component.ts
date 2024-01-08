44f50ea29d2c2816be9e0dd45bb6f4a43a91dfcf
ch.saagar <ch.saagarsingh@gmail.com>
2024-01-08 11:49:33 +0530

    data bind
    Signed-off-by: ch.saagar <ch.saagarsingh@gmail.com>

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'learn-Databinding';
+  firstname = "Shiva Tiwari"
+  imagepath = "assets/tekizma_inc_tekizma_bi_image.png"
+  defaultVal = "Tekizma"
+
+  passValueToComponent(e:any){
+    console.log(e.target.value);
+  }
+}
